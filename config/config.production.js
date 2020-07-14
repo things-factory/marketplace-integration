91d815e94a75cd0c95b7942cb2ad68869f3b3bfd
Hearty Oh <heartyoh@hatiolab.com>
2020-07-15 00:37:33 +0900

    add config

---
+++ config/config.production.js
@@ -0,0 +1,15 @@
+module.exports = {
+  'market-platform-lazada': {
+    platform: 'lazada',
+    application: 'operato-mms',
+    appKey: '111111',
+    appSecret: 'SSSSSSSS',
+    callback: 'https://mms.opa-x.com/callback-lazada'
+  },
+  'market-platform-shopee': {
+    platform: 'shopee',
+    application: 'Operato MMS',
+    partnerId: '888888',
+    partnerKey: 'qqqqqqqq'
+  }
+}
