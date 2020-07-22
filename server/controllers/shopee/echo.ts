10844525bcf3f006d2199944bc7d235e1e0e9d80
Hearty, Oh <heartyoh@hatiolab.com>
2020-07-22 09:52:02 +0900

    remove console log

--- server/controllers/shopee/echo.ts
+++ server/controllers/shopee/echo.ts
@@ -2,11 +2,9 @@ export function echo() {
   return {
     path: '/echo',
     denormalize(req) {
-      console.log('denormalize', req)
       return { ...req }
     },
     normalize(res) {
-      console.log('normalize', res)
       return { ...res }
     },
     docall(store, path, req, apicaller) {
--- server/index.ts
+++ server/index.ts
@@ -1,3 +1,6 @@
+import Debug from 'debug'
+const debug = Debug('things-factory:marketplace-integration')
+
 export * from './entities'
 export * from './migrations'
 export * from './graphql'
@@ -9,12 +12,12 @@ import './engine'
 import { Connections, ScenarioEngine } from '@things-factory/integration-base'
 
 process.on('bootstrap-module-start' as any, async ({ app, config, client }: any) => {
-  console.log('%%%%%%%%%%%%%%%% TASK ENGINE - BEGIN %%%%%%%%%%%%%%%%')
+  debug('%%%%%%%%%%%%%%%% TASK ENGINE - BEGIN %%%%%%%%%%%%%%%%')
   try {
     await Connections.ready()
     await ScenarioEngine.loadAll()
   } catch (ex) {
     Connections.logger.error(ex)
   }
-  console.log('%%%%%%%%%%%%%%%% TASK ENGINE - END %%%%%%%%%%%%%%%%')
+  debug('%%%%%%%%%%%%%%%% TASK ENGINE - END %%%%%%%%%%%%%%%%')
 })
