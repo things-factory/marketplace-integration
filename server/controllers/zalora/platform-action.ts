de98ef2f96f710f2e372680f53d765f9efa3729e
Hearty Oh <heartyoh@hatiolab.com>
2020-07-25 11:22:43 +0900

    zalora test

--- server/controllers/zalora/platform-action.ts
+++ server/controllers/zalora/platform-action.ts
@@ -4,7 +4,7 @@ export const action = async ({ store, path, request }) => {
   const client = new Zalora({
     apiKey: store.accessToken,
     userId: store.storeId,
-    countryCode: store.countryCode.toLowerCase()
+    countryCode: store.countryCode?.toLowerCase()
   })
 
   var response = await client.post(path, request)
--- test/zalora/test-store.ts
+++ test/zalora/test-store.ts
@@ -1,5 +1,13 @@
+import { config } from '@things-factory/env'
+const zaloraConfig = config.get('marketplaceIntegrationZalora', {})
+const { userId, apiKey, countryCode } = zaloraConfig
+
+/**
+ * zalora 셀러 정보는 configuration에서 지정하도록 함.
+ */
 export const store = {
   platform: 'zalora',
-  storeId: 'storeandsend@gmail.com',
-  countryCode: 'my'
+  storeId: userId,
+  accessToken: apiKey,
+  countryCode
 }
