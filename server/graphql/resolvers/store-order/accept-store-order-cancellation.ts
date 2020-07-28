217288163922d1335b3e1499b55c1570e8fc5076
horwengliang <horwengliang@gmail.com>
2020-07-28 18:35:58 +0800

    Update accept-store-order-cancellation.ts

--- server/graphql/resolvers/store-order/accept-store-order-cancellation.ts
+++ server/graphql/resolvers/store-order/accept-store-order-cancellation.ts
@@ -1,7 +1,10 @@
 import { config } from '@things-factory/env'
+import { StoreAPI } from 'server/controllers/store-api'
 
 export const acceptStoreOrderCancellation = {
-  async acceptStoreOrderCancellation(_: any, {}, context: any) {
+  async acceptStoreOrderCancellation(_: any, { storeId, orderId }, context: any) {
     // only in Shopee, to accept buyer order cancellation
+    var store = await StoreAPI.getMarketplaceStore(storeId)
+    return await StoreAPI.acceptStoreOrderCancellation(store, orderId)
   }
 }
