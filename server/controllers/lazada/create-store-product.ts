138d33643b72dcf088d398c597336fca09b8cf5c
horwengliang <horwengliang@gmail.com>
2020-07-29 13:05:47 +0800

    create and update product in Lazada

--- server/controllers/lazada/create-store-product.ts
+++ server/controllers/lazada/create-store-product.ts
@@ -3,11 +3,53 @@ export function createStoreProduct() {
     method: 'post',
     path: '/product/create',
     denormalize(req) {
-      var { payload } = req
-      return payload
+      var { categoryId: primary_category, attributes, skus } = req
+
+      skus = skus.map(sku => {
+        const {
+          skuId: sellerSku,
+          price,
+          quantity,
+          discountedPrice: special_price,
+          discountFromDate: special_from_date,
+          discountToDate: special_to_date,
+          weight: product_weight,
+          packageWidth: package_weight,
+          packageHeight: package_height,
+          packageLength: package_length,
+          packageContent: package_content,
+          images
+        } = sku
+        return {
+          sellerSku,
+          price,
+          special_price,
+          special_from_date,
+          special_to_date,
+          quantity,
+          product_weight,
+          package_weight,
+          package_height,
+          package_length,
+          package_content,
+          images
+        }
+      })
+
+      var payload = {
+        primary_category,
+        attributes,
+        skus
+      }
+
+      return { payload }
     },
     normalize(res) {
-      return res
+      var { item_id, sku_list } = res
+      return {
+        item_id,
+        sku_list
+      }
     }
   }
 }
--- server/controllers/lazada/update-store-product.ts
+++ server/controllers/lazada/update-store-product.ts
@@ -3,8 +3,46 @@ export function updateStoreProduct() {
     method: 'post',
     path: '/product/update',
     denormalize(req) {
-      var { payload } = req
-      return payload
+      var { categoryId: primary_category, attributes, skus } = req
+
+      skus = skus.map(sku => {
+        const {
+          skuId: sellerSku,
+          price,
+          quantity,
+          discountedPrice: special_price,
+          discountFromDate: special_from_date,
+          discountToDate: special_to_date,
+          weight: product_weight,
+          packageWidth: package_weight,
+          packageHeight: package_height,
+          packageLength: package_length,
+          packageContent: package_content,
+          images
+        } = sku
+        return {
+          sellerSku,
+          price,
+          special_price,
+          special_from_date,
+          special_to_date,
+          quantity,
+          product_weight,
+          package_weight,
+          package_height,
+          package_length,
+          package_content,
+          images
+        }
+      })
+
+      var payload = {
+        primary_category,
+        attributes,
+        skus
+      }
+
+      return ({ ...payload } = payload)
     },
     normalize(res) {
       return res
